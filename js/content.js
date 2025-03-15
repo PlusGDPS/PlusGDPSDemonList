2cbdefc6b759323f9e3ea2346484d09811f87fe7
zcharo99 <discoalt6969@gmail.com>
2025-03-15 21:46:48 +0100

    bro

--- js/content.js
+++ js/content.js
@@ -14,7 +14,6 @@ export async function fetchList() {
                 const levelResult = await fetch(`${dir}/${path}.json`);
                 try {
                     const level = await levelResult.json();
-                    console.log(level);
                     return [
                         {
                             ...level,
